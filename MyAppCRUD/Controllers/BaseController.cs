84ce2c841e326ef91f46dc82f604faebd05e9c40
slashcan <52168650+slashcan@users.noreply.github.com>
2024-03-03 15:19:35 +0800

    Add project files.

---
+++ MyAppCRUD/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyAppCRUD.Repository;
+
+namespace MyAppCRUD.Controllers
+{
+    public class BaseController : Controller
+    {
+        public WEBDBSYSEntities _db;
+        public BaseRepository<UserAccount> _userRepo;
+        public BaseController()
+        {
+            _db = new WEBDBSYSEntities();
+            _userRepo = new BaseRepository<UserAccount>();
+        }
+    }
+}
---
+++ MyAppCRUD/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MyAppCRUD.Controllers
+{
+    [Authorize(Roles = "User, Manager")]
+    public class HomeController : BaseController
+    {
+        // GET: Home
+
+        public ActionResult Index()
+        {
+            List<UserAccount> userList = _userRepo.GetAll();
+            return View(userList);
+        }
+
+        [Authorize(Roles = "Manager")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserAccount u)
+        {
+            _userRepo.Create(u);
+            TempData["Msg"] = $"User {u.username} added!";
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(_userRepo.Get(id));
+        }
+
+        [Authorize(Roles = "Manager")]
+        public ActionResult Edit(int id)
+        {
+
+            return View(_userRepo.Get(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UserAccount u)
+        {
+            _userRepo.Update(u.id, u);
+            TempData["Msg"] = $"User {u.username} updated!";
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize(Roles = "Manager")]
+        public ActionResult Delete(int id)
+        {
+            _userRepo.Delete(id);
+            TempData["Msg"] = $"User deleted!";
+
+            return RedirectToAction("Index");
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index");
+            return View();
+        }
+
+   
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(UserAccount u)
+        {
+            var user = _userRepo.Table.Where(m => m.username == u.username).FirstOrDefault();
+            if(user != null)
+            {
+                FormsAuthentication.SetAuthCookie(u.username, false);
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "User not Exist or Incorrect Password");
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+
+
+
+    }
+}
